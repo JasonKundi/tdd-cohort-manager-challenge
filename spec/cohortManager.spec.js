f271c452e1cf82eeb0f4c845dccee2ba8f02e801
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-01-24 23:31:33 +0000

    Working on remove method now, thinking of scrapping loop approach and trying other short hand ways to implement

--- spec/cohortManager.spec.js
+++ spec/cohortManager.spec.js
@@ -1,64 +1,79 @@
-const CohortManager = require("../src/cohortManager.js")
-const Cohort = require("../src/Cohort.js")
+const CohortManager = require("../src/cohortManager.js");
+const Cohort = require("../src/Cohort.js");
 
 describe("TodoList", () => {
-    let cohortManager
+  let cohortManager;
+
+  beforeEach(() => {
+    cohortManager = new CohortManager();
+  });
+
+  it("CohortManager//createCohort() - creates a new instance of the Cohort class by name", () => {
+    //set up
+    cohortManager.createCohort("A Class");
+
+    //expected
+
+    const expected = [new Cohort("A Class")];
+
+    const result = cohortManager.cohorts;
+
+    expect(result).toEqual(expected);
+  });
+  it("CohortManager//searchForCohort() - searches for Cohort class by name", () => {
+    //set up
+    cohortManager.createCohort("A Class");
+    cohortManager.createCohort("B Team");
+    cohortManager.createCohort("C List");
+
+    //expected
+
+    const expected = new Cohort("B Team");
+
+    const result = cohortManager.searchForCohort("B Team");
+
+    expect(result).toEqual(expected);
+  });
+  it("CohortManager//returnAllCohorts() - returns all Cohorts", () => {
+    //set up
+    const class1 = new Cohort("A Class");
+    const class2 = new Cohort("B Team");
+    const class3 = new Cohort("C List");
+
+    cohortManager.createCohort("A Class");
+    cohortManager.createCohort("B Team");
+    cohortManager.createCohort("C List");
+
+    cohortManager.returnAllCohorts()
+
+    //expected
+
+    const expected = [class1, class2, class3]
+
+    const result = cohortManager.returnAllCohorts("A Class","B Team","C List");
+
+    expect(result).toEqual(expected);
+  });
+  it ("CohortManager//removeCohort() - removes cohort by name", () => {
   
-    beforeEach(() => {
-        cohortManager = new CohortManager()
-    })
-    
-    it ("CohortManager//createCohort() - creates a new instance of the Cohort class by name", () => {
-
-        //set up
-        cohortManager.createCohort("A Class")
-
-        //expected
-
-        const expected = [new Cohort("A Class")]
-
-        const result = cohortManager.cohorts
-        
-        expect(result).toEqual(expected)
-    })
-    it ("CohortManager//searchForCohort() - searches for Cohort class by name", () => {
-
-        //set up
-        cohortManager.createCohort("A Class")
-        cohortManager.createCohort("B Team")
-        cohortManager.createCohort("C List")
-
-
-        //expected
-
-        const expected = new Cohort("B Team")
-
-        const result = cohortManager.searchForCohort("B Team")
-        
-        expect(result).toEqual(expected)
-    })
-    //it ("CohortManager//removeCohort() - removes cohort by name", () => {
-    //    
-    //    //set up
-    //    const class1 = Cohort("A Class")
-    //    const class2 = Cohort("B Team")
-    //    const class3 = Cohort("C List")
-//
-    //    cohortManager.createCohort("A Class")
-    //    cohortManager.createCohort("B Team")
-    //    cohortManager.createCohort("C List")
-//
-    //    cohortManager.removeCohort("C List")
-//
-//
-    //    //expected
-//
-    //    const expected = [class1, class2]
-//
-    //    const result = cohortManager.getAllCohorts()
-    //    
-    //    expect(result).toEqual(expected)
-    //})
-
-
-})
+      //set up
+      const class1 = new Cohort("A Class")
+      const class2 = new Cohort("B Team")
+      const class3 = new Cohort("C List")
+  
+      cohortManager.createCohort("A Class")
+      cohortManager.createCohort("B Team")
+      cohortManager.createCohort("C List")
+  
+      cohortManager.removeCohort("C List")
+  
+  
+      //expected
+  
+      const expected = [class1, class2]
+  
+      const result = cohortManager.returnAllCohorts()
+  
+      expect(result).toEqual(expected)
+  })
+});
--- src/cohortManager.js
+++ src/cohortManager.js
@@ -1,29 +1,31 @@
-const Cohort = require("./Cohort.js")
+const Cohort = require("./Cohort.js");
 
 class CohortManager {
-    constructor() {
-        this.cohorts =[]
+  constructor() {
+    this.cohorts = [];
+  }
 
+  createCohort(nameOf) {
+    const cohort = new Cohort(nameOf);
+    this.cohorts.push(cohort);
+    return this.cohorts;
+  }
+  searchForCohort(nameOf) {
+    const searchCohort = this.cohorts.find((cohort) => cohort.name === nameOf);
+    return searchCohort;
+  }
+  returnAllCohorts() {
+    return this.cohorts;
+  }
+
+  removeCohort(removeName) {
+    for (let i = 0; i < this.cohorts.length; i++) {
+      if (this.cohorts[i] === removeName) {
+         this.cohorts[i].splice;
+      }
+      return this.cohorts
     }
-
-    createCohort(nameOf) {
-        const cohort = new Cohort(nameOf)
-        this.cohorts.push(cohort)
-        return this.cohorts
-
-    }
-    searchForCohort(nameOf) {
-        const searchCohort = this.cohorts.find((cohort) => cohort.name === nameOf);
-        return searchCohort
-        
-
-    }
-    
-    //for (let i = 0; i < this.cohorts.length; i++) {
-    //    if (this.cohorts[i] === nameOf) {
-    //        return this.cohorts[i]
-    //    }
-   //}
+  }
 }
 
 module.exports = CohortManager;
